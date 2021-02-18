aab87330607747e8a6761e2532933a9fede8489d
ForTombstone <66000635+Nefomemes@users.noreply.github.com>
2021-02-18 15:06:39 +0700

    Update blclient.js

--- src/classes/blclient.js
+++ src/classes/blclient.js
@@ -1,11 +1,21 @@
 
 const utils = require("../utils/utils");
 const { GameClient } = require("./gameclient");
-
+/**
+* Represents a Battlelog session.
+* @class
+*/
 class BattlelogClient {
 	type = "BattlelogClient"
 	
 	
+
+
+/**
+* Creates a new Battlelog session.
+* @constructor
+* @param {object} options- The options used here.
+*/
 	constructor(options = {}){
 		if(options && typeof options !== "object"){
 			throw Error(`Parameter 'options' is required to be an object.  While it is ${utils.getArticle(typeof options)} ${typeof options}.`);
@@ -14,6 +24,10 @@ class BattlelogClient {
 
 	}
 	
+/**
+* Creates a new GameClient instance.
+* @param {...*} params - The params used to create the instance.
+*/
 	game(...params) {
 		return new GameClient(this, ...params);
 
